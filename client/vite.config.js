62e487b71c96d542973efc0c72cacdf270060466
DanielFelix-eng <felixjardini502@gmail.com>
2026-07-20 14:44:46 +0300

    fixed prototype

--- client/vite.config.js
+++ client/vite.config.js
@@ -18,7 +18,6 @@ export default defineConfig({
        '/api': {
          target: 'http://127.0.0.1:3000',
          changeOrigin: true,
-         cookieDomainRewrite: 'localhost',
        }
      }
    }
